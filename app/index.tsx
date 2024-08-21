c0eaa48f861edcf739bcc4091e4a3bb5006647b9
jasmithadodda <jasmithadodda@gmail.com>
2024-08-21 12:55:17 +0530

    buttons

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,43 @@
-import { StyleSheet, Text, View,Image } from "react-native";
-const styles = StyleSheet.create({
- 
-});
-export default function Index() {
-  return (
-    <>
- <Image source={require("../assets/images/react-logo.png")} />
-    
-    </>
-  );
+import React, {Component} from 'react';
+import {Alert, Button, StyleSheet, View} from 'react-native';
+
+export default class ButtonBasics extends Component {
+  _onPressButton() {
+    Alert.alert('You tapped the button!');
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button onPress={this._onPressButton} title="Press Me" />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+            color="#841584"
+          />
+        </View>
+        <View style={styles.alternativeLayoutButtonContainer}>
+          <Button onPress={this._onPressButton} title="This looks great!" />
+          <Button onPress={this._onPressButton} title="OK!" color="#841584" />
+        </View>
+      </View>
+    );
+  } 
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 20,
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
